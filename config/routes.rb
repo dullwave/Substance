8617e6ef97cb4d673506359021bb86245d6e13f4
Elisabeth Waltz <elisabeth.waltz@gmail.com>
2014-03-27 15:59:52 -0400

    Added posts to routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Substance::Application.routes.draw do
 
   resources :users, :only => [:show] do
     resources :follows, :only => [:create, :destroy]
+    resources :posts
   end
 
   root to: 'home#index'
